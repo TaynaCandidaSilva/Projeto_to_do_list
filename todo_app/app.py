2c294281e976b554bc734751cd90ac031b873f1b
TaynaCandidaSilva~ <tayna2507@gmail.com>
2025-02-13 14:00:46 -0300

    Implementacao: inicializacao do servidor Flask para rodar a aplicação

---
+++ todo_app/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+from database import db, app
+from models import Task
+
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True)
