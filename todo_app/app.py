4aa4cf541e0d66acea2a0541a3b4a53b844fa98d
TaynaCandidaSilva~ <tayna2507@gmail.com>
2025-02-14 14:18:20 -0300

    Implementando Blueprint as rotas

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,9 +1,10 @@
 from database import db, app
-from models import Task
+from routes import routes
 
+app.register_blueprint(routes)
 
 with app.app_context():
-    db.create_all()
-
+    db.create_all() 
+   
 if __name__ == "__main__":
     app.run(debug=True)
